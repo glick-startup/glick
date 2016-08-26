1bf85d1ad2c0944187148a3befc8ff9c9a3ae904
Martinho Aragão <martinhoaragao@gmail.com>
2016-08-26 14:59:40 +0100

    Move homepage data to separate file

---
+++ cards.js
@@ -0,0 +1,41 @@
+const cards = [
+  {
+    type: 'question',
+    text: 'Do you have rent?'
+  },
+  {
+    type: 'question',
+    text: 'Was this a restaurant?'
+  },
+
+  {
+    type: 'feedback',
+    text: 'You have won 200 points the last month. Well done'
+  },
+  {
+    type: 'feedback',
+    text: 'Keep up the good work, you are awesome!'
+  },
+
+  {
+    type: 'location',
+    location: 'Starbucks Belém',
+    text: 'Do not buy anything! It is above your possibilities!'
+  },
+  {
+    type: 'location',
+    location: 'Pingo Doce - Vilar dos Chãos',
+    text: 'Feel free to buy a chocolate, or 2!'
+  },
+
+  {
+    type: 'suggestion',
+    text: 'You should spend less in chocolate.'
+  },
+  {
+    type: 'suggestion',
+    text: 'Are you sure you need more candy?'
+  }
+]
+
+export default cards;
--- home.js
+++ home.js
@@ -1,32 +1,12 @@
-SuggestionCards = new Mongo.Collection('suggestions_cards');
-
-
-QuestionCards = new Mongo.Collection('questions_cards');
-
-FeedbackCards = new Mongo.Collection('feedback_cards');
-
-LocationCards = new Mongo.Collection('locations_cards');
-const locationCards = [
-  {
-    location: 'Starbucks Belém',
-    text: 'Do not buy anything! It is above your possibilities!'
-  },
-  {
-    location: 'Pingo Doce - Vilar dos Chãos',
-    text: 'Feel free to buy a chocolate, or 2!'
-  }
-]
-LocationCards.insert(locationCards);
-
-
-Cards = new Mongo.Collection('cards');
-
+import Cards from './cards'
 
 if(Meteor.isClient){
+  var numberOfCards = Math.floor(Math.random() * Cards.length) + 1;
+
   // client code goes here
   Template.cards.helpers({
     'card': function(){
-      return Cards.find()
+      return Cards
     }
   })
 }
