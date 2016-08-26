24118caa61cd98f5d7b64bb5d443fefbeac82cca
Martinho Aragão <martinhoaragao@gmail.com>
2016-08-26 18:18:25 +0100

    Add curent balance random

--- expenses.js
+++ expenses.js
@@ -1,12 +1,18 @@
 import Expenses_groups from './expenses_groups.js'
 
 if(Meteor.isClient){
-  
+
   // client code goes here
   Template.expenses_list.helpers({
     'expense': function(){
       expenses_group = Math.floor(Math.random() * 3);
       return Expenses_groups[expenses_group]
+    }
+  })
+
+  Template.expenses.helpers({
+    'value': function() {
+      return (Math.floor(Math.random() * 10000))
     }
   })
 }
