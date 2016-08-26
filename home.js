92821d3a55c0f0a5ee2506636b4dcaa86a095099
Martinho Aragão <martinhoaragao@gmail.com>
2016-08-26 10:50:05 +0100

    Begin Database for homepage

--- home.js
+++ home.js
@@ -1,4 +1,26 @@
-Cards = new Mongo.Collection('cards')
+SuggestionCards = new Mongo.Collection('suggestions_cards');
+
+
+QuestionCards = new Mongo.Collection('questions_cards');
+
+FeedbackCards = new Mongo.Collection('feedback_cards');
+
+LocationCards = new Mongo.Collection('locations_cards');
+const locationCards = [
+  {
+    location: 'Starbucks Belém',
+    text: 'Do not buy anything! It is above your possibilities!'
+  },
+  {
+    location: 'Pingo Doce - Vilar dos Chãos',
+    text: 'Feel free to buy a chocolate, or 2!'
+  }
+]
+LocationCards.insert(locationCards);
+
+
+Cards = new Mongo.Collection('cards');
+
 
 if(Meteor.isClient){
   // client code goes here
