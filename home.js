1e409571d3be3b465c7b1b727e3d7479c43baa3b
Martinho Aragão <martinhoaragao@gmail.com>
2016-08-26 15:46:43 +0100

    Replace package.json to meteor packages

--- home.js
+++ home.js
@@ -1,12 +1,23 @@
 import Cards from './cards'
 
-if(Meteor.isClient){
-  var numberOfCards = Math.floor(Math.random() * Cards.length) + 1;
+function getRandomSubarray(arr) {
+    const size = Math.floor(Math.random() * arr.length) + 1;
+    var shuffled = arr.slice(0), i = arr.length, min = i - size, temp, index;
+    while (i-- > min) {
+        index = Math.floor((i + 1) * Math.random());
+        temp = shuffled[index];
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+    }
+    return shuffled.slice(min);
+}
 
+
+if(Meteor.isClient){
   // client code goes here
   Template.cards.helpers({
     'card': function(){
-      return Cards
+      return getRandomSubarray(Cards)
     }
   })
 }
