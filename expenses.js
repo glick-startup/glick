ec9de5452c352d24dafa128da4687c19fde87ecd
dimesq <d.mesquita285@gmail.com>
2016-08-26 14:34:26 +0100

    Adds expenses basics

---
+++ expenses.js
@@ -0,0 +1,28 @@
+Expenses = new Mongo.Collection('expenses')
+
+if(Meteor.isClient){
+  const shopaholic_expenses = [
+    {
+     type: "shopping",
+     value: "1000€",
+     date: "2016-08-24"
+    },
+    {
+     type: "rent",
+     value: "300€",  
+     date: "2016-08-25"
+    }
+  ]
+  // client code goes here
+  Template.expenses_list.helpers({
+    'expense': function(){
+      return shopaholic_expenses
+    }
+  })
+}
+
+if(Meteor.isServer){
+  // server code goes here
+}
+
+//Expenses.remove({})
